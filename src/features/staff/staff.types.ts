99c95c0b9a8e008c1dee5f2421839ee445fd7cc9
M1kYbaD <167187226+M1kYbaD@users.noreply.github.com>
2025-12-16 10:47:28 +0100

    update attribute description

--- src/features/staff/staff.types.ts
+++ src/features/staff/staff.types.ts
@@ -1,7 +1,7 @@
 export interface MedicalStaff {
   id: number; // identificatore univoco membro
   name: string; // nome + cognome
-  date_of_birth: string; // formato ISO 8601
+  date_of_birth: string; // formato standard ISO 8601
   role: string; // ruolo
   department: string; // reparto + codice unico (R...)
 }
